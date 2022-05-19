0cd9c51df57c0e35aea9397c09467ee02453b3b4
Essejblack <ehsan.jnra@gmail.com>
2022-05-19 20:47:37 +0430

    add camera , scene , safhe , sayeh , render function

--- scripts.js
+++ scripts.js
@@ -1,8 +1,25 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+)
+const renderer = new THREE.WebGLRenderer()
 
-function init(){
+function init() {
 
+      renderer.setClearColor(0xffffff, 1.0) // background safhe
+      renderer.setSize(window.innerWidth, window.innerHeight) // size safhe
+      renderer.shadowMap.enabled = true // sayeh 
+
+      
+
+      document.getElementById("WebGL-output").appendChild(renderer.domElement)
+
+      render = function () {
+            renderer.render(scene, camera)
+      }
+      render()
 }
-window.onload = init;
+window.onload = init
