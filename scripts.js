05330fae3c47705b28087275580de19e1fe5ed57
Essejblack <ehsan.jnra@gmail.com>
2022-05-25 20:32:03 +0430

    adding plane floor

--- scripts.js
+++ scripts.js
@@ -8,18 +8,26 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer()
 
 function init() {
-
       renderer.setClearColor(0xffffff, 1.0) // background safhe
       renderer.setSize(window.innerWidth, window.innerHeight) // size safhe
-      renderer.shadowMap.enabled = true // sayeh 
+      renderer.shadowMap.enabled = true // sayeh
 
       camera.position.x = -60
       camera.position.y = 60
       camera.position.z = 0
       camera.lookAt(scene.position)
 
-      
-      
+      // kaf
+      const planeGeometry = new THREE.PlaneGeometry(100, 100, 1, 1)
+      const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xcccccc })
+      const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+
+      plane.rotation.x = -0.5 * Math.PI
+      plane.position.x = 15
+      plane.position.y = 0
+      plane.position.z = 0
+      plane.receiveShadow = true 
+      scene.add(plane)
 
       document.getElementById("WebGL-output").appendChild(renderer.domElement)
 
